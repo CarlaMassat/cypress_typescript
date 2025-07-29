db5d89d6ead2102055976eb41fe05961a34cefb0
CarlaMassat <cm90mdp@gmail.com>
2025-07-29 09:52:35 -0300

    Added  cypress.config.ts,  cypress/support/e2e.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  waitForAnimations: true,
+  watchForFileChanges: false,
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  defaultCommandTimeout: 8000,
+  reporter: 'mochawesome',
+  retries: { openMode: 0, runMode: 4 },
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
+    baseUrl: 'https://practice.automationtesting.in/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,3 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+Cypress.on('uncaught:exception', (err) => {
+  if (err.message.includes('adsbygoogle.push() error')) {
+    return false 
+  }
+
+  return true
+});
