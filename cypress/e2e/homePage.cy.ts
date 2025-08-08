ecc6046277cc35630581dca3af583c600f0439d4
CarlaMassat <cm90mdp@gmail.com>
2025-08-08 10:36:50 -0300

    Changes on homePage tests

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -30,4 +30,29 @@ describe("Home Page", function () {
     productPage.getAddToBasket().should('exist');
   });
 
+  it("Arrivals-Images-Description", function () {
+    const imageNumber = Math.floor(Math.random()* 3) + 1
+    homePage.getNewArrivals().should('have.length', 3);
+    homePage.getOneImageInArrivals(imageNumber).click();
+    productPage.getAddToBasket().should('exist');
+    productPage.getDescriptionTab().click();
+    productPage.getDescription().should('be.visible');
+  });
+
+   it("Arrivals-Images-Reviews", function () {
+
+    const imageNumber = Math.floor(Math.random()* 3) + 1
+    homePage.getNewArrivals().should('have.length', 3);
+    homePage.getOneImageInArrivals(imageNumber).click();
+    productPage.getAddToBasket().should('exist');
+    productPage.getReviewTab().click();
+    productPage.getReviewCount().then((text) =>{
+    const match = text.match(/\((\d+)\)/);
+    const count = parseInt(match[1], 10);
+    expect(count).to.equal(0);
+    });
+    productPage.getReview().should('be.visible');
+
+  });
+
 });
